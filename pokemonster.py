c73ed8bf435536db4edcdfb10f04ab02468af1fe
Ubuntu <ubuntu@ip-172-31-39-173.ec2.internal>
2020-09-24 04:26:37 +0000

    types and sprites

--- pokemonster.py
+++ pokemonster.py
@@ -7,16 +7,25 @@ class Pokemonster:
         self.link = f"http://pokeapi.co/api/v2/pokemon/{self.name}"
         self.data = json.loads(requests.get(self.link).text)
         self.get_abilities()
+        self.get_types()
         
-    
     def get_abilities(self):
         self.abilities =  [ab["ability"]["name"] for ab in self.data["abilities"]]
-        
     
+        
+    def get_sprite(self, version):
+        sprites = self.data["sprites"]
+        self.sprite = sprites[version]
+        
+    def get_types(self):
+        types = self.data["types"]
+        self.types = [type["type"]["name"] for type in types]
+    
+    @property
     def to_string(self):
-        print(self.name, self.abilities)
+        return (self.name, self.abilities, self.types)
         
         
         
 bulbasaur = Pokemonster("bulbasaur")
-bulbasaur.to_string()
+print(bulbasaur.to_string)
