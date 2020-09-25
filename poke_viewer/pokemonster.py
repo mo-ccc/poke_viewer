856b81970e3357636f51daa0a56227d394dc53b9
Ubuntu <ubuntu@ip-172-31-39-173.ec2.internal>
2020-09-25 03:53:50 +0000

    pep8 compliance

--- poke_viewer/pokemonster.py
+++ poke_viewer/pokemonster.py
@@ -9,36 +9,34 @@ class Pokemonster:
         self.get_abilities()
         self.get_types()
         self.get_default_and_shiny_sprite()
-        
+
     def get_data(self):
         try:
             data = requests.get(self.link).text
             self.data = json.loads(data)
-        except:
+        except (AttributeError, json.decoder.JSONDecodeError):
             raise Exception("We were unable to find that pokemon")
-            
-        
+
     def get_abilities(self):
-        self.abilities =  [ab["ability"]["name"] for ab in self.data["abilities"]]
-    
-        
+        self.abilities =  [ab["ability"]["name"]
+                            for ab in
+                            self.data["abilities"]]
+
     def get_sprite_of_type(self, version: str):
         sprites = self.data["sprites"]
         return sprites[version]
-        
+ 
     def get_default_and_shiny_sprite(self):
         self.sprites = (self.get_sprite_of_type("front_default"),
                         self.get_sprite_of_type("front_shiny"))
-        
+
     def get_types(self):
         types = self.data["types"]
         self.types = [type["type"]["name"] for type in types]
-    
+
     @property
     def to_string(self):
         return (self.name, self.abilities, self.types, self.sprites)
-        
-        
-        
+
 bulbasaur = Pokemonster("bulbasaur")
 print(bulbasaur.to_string)
