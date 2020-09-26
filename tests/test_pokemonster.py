888513c0e9a244d25f7339d8b2edcc7c46ef5aa6
Ubuntu <ubuntu@ip-172-31-39-173.ec2.internal>
2020-09-26 12:14:47 +0000

    added more to unittest

--- tests/test_pokemonster.py
+++ tests/test_pokemonster.py
@@ -2,6 +2,17 @@ from poke_viewer.pokemonster import Pokemonster
 import unittest
 
 class TestApi(unittest.TestCase):
-    def test_bulbasaur(self):
-        bulbasaur = Pokemonster("bulbasaur")
-        self.assertEqual(bulbasaur.types, ['grass', 'poison'])
+    bulbasaur = Pokemonster("bulbasaur")
+    def test_api_types(self):
+        self.assertEqual(self.bulbasaur.types, ['grass', 'poison'])
+    
+    def test_api_abilities(self):
+        self.assertEqual(self.bulbasaur.abilities, ['overgrow', 'chlorophyll'])
+    
+    def test_api_sprite(self):
+        self.assertEqual(self.bulbasaur.sprites, 
+        ["https://raw.githubusercontent.com/PokeAPI/sprites/"
+         "master/sprites/pokemon/1.png",
+         "https://raw.githubusercontent.com/PokeAPI/sprites/"
+         "master/sprites/pokemon/shiny/1.png"])
+        
